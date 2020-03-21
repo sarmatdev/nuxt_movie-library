4b7f70ac1a64aa9bac605b52e6cde5155c45f477
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-21 16:49:04 +0300

    Create link component

---
+++ components/Link.vue
@@ -0,0 +1,31 @@
+<template>
+  <nuxt-link class="link" to="/">
+    <font-awesome-icon :icon="['fas', 'dot-circle']" />
+    <slot></slot>
+  </nuxt-link>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.link {
+  text-decoration: none;
+  color: var(--color-primary-dark);
+  transition: all 0.2s ease;
+  display: block;
+
+  &:visited {
+    color: var(--color-primary-dark);
+  }
+
+  &:hover {
+    transform: translateY(-2px);
+  }
+
+  &:active {
+    transform: translateY(2px);
+  }
+}
+</style>
